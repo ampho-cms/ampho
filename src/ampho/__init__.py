f9152a52c55ffdf0578b579e243053dcfbba709a
Oleksandr Shepetko <a@shepetko.com>
2020-02-15 18:49:29 +0200

    `app` renamed to `current_app`

--- src/ampho/__init__.py
+++ src/ampho/__init__.py
@@ -1,4 +1,4 @@
-"""Ampho
+"""Ampho Init
 """
 __description__ = 'Ampho is the core part of the Ampho CMS'
 __author__ = 'Oleksandr Shepetko'
@@ -7,6 +7,6 @@ __license__ = 'MIT'
 __version__ = '0.2'
 
 # Public API
-from ._api import app, get_caller_bundle, request, route, render
+from ._api import current_app, get_caller_bundle, request, route, render
 from ._application import Application
 from ._bundle import Bundle
