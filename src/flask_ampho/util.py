8b41d79eb89e2416766f0d1d9d47be0e6301472c
Alexander Shepetko <a@shepetko.com>
2020-08-03 19:48:14 +0300

    New function util.is_dir_empty()

--- src/flask_ampho/util.py
+++ src/flask_ampho/util.py
@@ -7,19 +7,38 @@ __license__ = 'MIT'
 import pkgutil
 import logging
 from typing import Union, List
-from os import path
+from os import path, listdir
+from fnmatch import fnmatch
 from click import secho
 
 
-def package_path(pkg_name: str, subdir: Union[str, List[str]]):
+def package_path(pkg_name: str, subdir: Union[str, List[str]] = None):
     pkg_info = pkgutil.get_loader(pkg_name)
     if not pkg_info:
         raise ModuleNotFoundError(f"Package {pkg_name} is not found")
 
-    if isinstance(subdir, str):
+    if subdir is None:
+        subdir = []
+    elif isinstance(subdir, str):
         subdir = [subdir]
 
     return path.join(path.dirname(pkg_info.path), *subdir)
+
+
+def is_dir_empty(dir_path: str, exclude: Union[str, List[str]] = None) -> bool:
+    """Check if the directory is empty
+    """
+    if exclude is None:
+        exclude = ['__pycache__']
+    elif isinstance(exclude, str):
+        exclude = [exclude]
+
+    for fn in listdir(dir_path):
+        for pat in exclude:
+            if not fnmatch(fn, pat):
+                return False
+
+    return True
 
 
 def secho_info(msg: str, *args, **kwargs):
